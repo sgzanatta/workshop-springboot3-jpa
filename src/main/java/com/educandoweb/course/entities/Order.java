aee2af258356ee06de3f0961dbb942dc9f52cb04
sgzanatta <sgzanatta92@gmail.com>
2024-04-10 17:20:36 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -19,33 +20,36 @@ import jakarta.persistence.Table;
 public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	//ATRIBUTOS
+
+	// ATRIBUTOS
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
-	//ASSOCIACOES
-	
+
+	private Integer orderStatus;
+
+	// ASSOCIACOES
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
-	//CONSTRUTORES
+
+	// CONSTRUTORES
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
-	
-	//GET E SET
+
+	// GET E SET
 	public Long getId() {
 		return id;
 	}
@@ -62,6 +66,16 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+
 	public User getClient() {
 		return client;
 	}
@@ -69,8 +83,8 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-	
-	//HASHCODE E EQUALS
+
+	// HASHCODE E EQUALS
 	@Override
 	public int hashCode() {
 		return Objects.hash(client, id, moment);
@@ -88,6 +102,5 @@ public class Order implements Serializable {
 		return Objects.equals(client, other.client) && Objects.equals(id, other.id)
 				&& Objects.equals(moment, other.moment);
 	}
-	
-	
+
 }
