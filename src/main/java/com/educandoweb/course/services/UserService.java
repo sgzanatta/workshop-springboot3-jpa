72be79e946e99478bd8b788e4df574c0cf42d454
sgzanatta <sgzanatta92@gmail.com>
2024-04-18 16:05:40 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -43,9 +43,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (jakarta.persistence.EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
